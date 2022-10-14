c5e374e0e3a91db09e12efd7914e584107c7e47e
hugosmoreira <hugo.n4@gmail.com>
2022-10-13 19:06:10 -0700

    header and layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,24 @@
 import Head from 'next/head'
 
 
-export default function Layout({ title, children }) {
+export default function Layout({ title, children, keywords, description }) {
     return (
         <div>
             <Head>
                 <title>{title}</title>
+                <meta name='keywords' content={keywords} />
+                <meta name='description' content={description} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>{children}</main>
         </div>
     )
+}
+
+Layout.defaultProps = {
+    title: 'WebDev News',
+    keywords: 'web development, programming',
+    description: 'Get the latest news in web dev',
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
+
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    </div>
+    </Layout>
   )
 }
